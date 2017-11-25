2153119d94819d6f89a089d158cd3cfb76c12f5b
dijksterhuis <dijksterhuis.music@gmail.com>
2017-11-25 03:39:03 +0000

    update pyton script

--- 2-data-load/redis_load.py
+++ 2-data-load/redis_load.py
@@ -6,7 +6,7 @@ r_p_checker = redis.Redis(host='redis-pw',port='6379')
 added = 0
 with open('10_million_password_list_top_1000000.txt') as f:
 	for idx, line in enumerate(f.readlines()):
-		added += r_p_checker.sadd(index,str(line.rstrip('\n')))
+		added += r_p_checker.sadd('1000000',str(line.rstrip('\n')))
 		console.write('\r + {} proc - {} add +'.format(idx, added) )
 		console.flush()
 print('\n +++ Load completed')
