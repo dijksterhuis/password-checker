66b3e0f047c980a70665f77794453859ee9a82dd
dijksterhuis <dijksterhuis.music@gmail.com>
2017-11-25 03:13:47 +0000

    typo in redis up-conatiner script was causing container to exit

--- 1-redis-cache/docker_run.sh
+++ 1-redis-cache/docker_run.sh
@@ -1,4 +1,4 @@
 name=redis-pw
 
 docker rm -f $name || true
-docker run -d -p 6379:6379 -v redis-pw:/data --name $name --network redis-pw redis:alpine redisserver --appendonly yes
+docker run --restart=always -p 6379:6379 -v redis-pw:/data --name $name --network redis-pw redis:alpine redis-server --appendonly yes
