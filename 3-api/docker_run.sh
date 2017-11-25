8060113661a04ffbcd8f7c18cb19e0b2ccb9d0f0
dijksterhuis <mrobeson@dundee.ac.uk>
2017-11-25 14:24:17 +0000

    force rest-api container onto localhost exposure

--- 3-api/docker_run.sh
+++ 3-api/docker_run.sh
@@ -1,1 +1,6 @@
-docker run -d --restart=always -v api-logs:/logs/ --network pw-checker-api --name pwchecker-flaskapi pwchecker:flask-api /bin/ash -c 'python /home/api.py'
+docker run -d --restart=always \
+    -v api-logs:/logs/ \
+        --network pw-checker-api \
+            --name pwchecker-flaskapi \
+                -p 127.0.0.1:100:80 \
+                pwchecker:flask-api /bin/ash -c 'python /home/api.py'
