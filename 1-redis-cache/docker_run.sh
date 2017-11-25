0281b12f38834c3785f3d746669ecb323506663a
dijksterhuis <dijksterhuis.music@gmail.com>
2017-11-25 03:02:55 +0000

    force remove and return true even on error when attemping removal

--- 1-redis-cache/docker_run.sh
+++ 1-redis-cache/docker_run.sh
@@ -1,4 +1,4 @@
 name=redis-pw
 
-docker stop $name ; docker rm $name
+docker rm -f $name || true
 docker run -d -p 6379:6379 -v redis-pw:/data --name $name --network redis-pw redis:alpine redisserver --appendonly yes
