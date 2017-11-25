dc49336b4ff6cd0269d1de2402ee5bc49d87b907
dijksterhuis <dijksterhuis.music@gmail.com>
2017-11-25 04:05:43 +0000

    add api files for next stage

--- jenkins/jenkins.sh
+++ jenkins/jenkins.sh
@@ -1,1 +1,1 @@
-docker run --rm -d -p 8080:8080 -v jenkins-vol:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --name jenkins-ci jenkinsci/blueocean
+docker run --rm -d -u root -p 8080:8080 -v jenkins-vol:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --name jenkins-ci jenkinsci/blueocean ; docker attach jenkins-ci
