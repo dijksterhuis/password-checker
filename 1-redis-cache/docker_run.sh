c2c37360819b118696d17b41c48ffa290318d722
dijksterhuis <dijksterhuis.music@gmail.com>
2017-11-25 03:01:03 +0000

    git rid of rm flag, doesn't work with d you numpty

--- 1-redis-cache/docker_run.sh
+++ 1-redis-cache/docker_run.sh
@@ -1,4 +1,4 @@
 name=redis-pw
 
 docker stop $name ; docker rm $name
-docker run -d --rm -p 6379:6379 -v redis-pw:/data --name $name --network redis-pw redis:alpine redisserver --appendonly yes
+docker run -d -p 6379:6379 -v redis-pw:/data --name $name --network redis-pw redis:alpine redisserver --appendonly yes
