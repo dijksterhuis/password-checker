04859036b3bf5648c129bb4ad0c6f99042ff17ae
dijksterhuis <mrobeson@dundee.ac.uk>
2017-11-25 14:25:32 +0000

    force container removal on rebuild

--- 3-api/docker_run.sh
+++ 3-api/docker_run.sh
@@ -1,6 +1,10 @@
-docker run -d --restart=always \
+docker rm -f pwchecker-flaskapi
+
+docker run \
+    -d \
+    --restart=always \
     -v api-logs:/logs/ \
-        --network pw-checker-api \
-            --name pwchecker-flaskapi \
-                -p 127.0.0.1:100:80 \
-                pwchecker:flask-api /bin/ash -c 'python /home/api.py'
+    --network pw-checker-api \
+    --name pwchecker-flaskapi \
+    -p 127.0.0.1:100:80 \
+        pwchecker:flask-api /bin/ash -c 'python /home/api.py'
