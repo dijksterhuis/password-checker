253f102bb5b33c1c0ede3d66565ead0b3099b73e
dijksterhuis <dijksterhuis.music@gmail.com>
2017-11-25 03:34:45 +0000

    download passwords file on build,

--- 2-data-load/redis_load.py
+++ 2-data-load/redis_load.py
@@ -4,7 +4,7 @@ from sys import stdout
 
 r_p_checker = redis.Redis(host='redis-pw',port='6379')
 added = 0
-with open('0-data/passwords-file.txt') as f:
+with open('10_million_password_list_top_1000000.txt') as f:
 	for idx, line in enumerate(f.readlines()):
 		added += r_p_checker.sadd(index,str(line.rstrip('\n')))
 		console.write('\r + {} proc - {} add +'.format(idx, added) )
