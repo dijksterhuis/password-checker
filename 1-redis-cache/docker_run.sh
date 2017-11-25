540392a49ac8880b63609077a53a4c5bcb9a4e8a
dijksterhuis <mrobeson@dundee.ac.uk>
2017-11-25 13:54:30 +0000

    change network connections for redis container

--- 1-redis-cache/docker_run.sh
+++ 1-redis-cache/docker_run.sh
@@ -1,4 +1,7 @@
 name=redis-pw
 
 docker rm -f $name || true
-docker run -d --restart=always -p 6379:6379 -v redis-pw:/data --name $name --network pw-checker-redis redis:alpine redis-server --appendonly yes
+docker run -d --restart=always -p 6379:6379 -v redis-pw:/data --name $name redis:alpine redis-server --appendonly yes
+
+docker network connect pw-checker-redis redis-pw
+docker network connect api redis-pw
