6173cb8f7d63387e4dea8ba8b5af0b7b1abe633d
Mike <dijksterhuis@users.noreply.github.com>
2017-12-03 02:06:05 +0000

    update echos

--- curl-test.sh
+++ curl-test.sh
@@ -1,17 +1,6 @@
-echo "------"
-echo "+++++++++++++++ Testing for a TRUE value"
-echo "------"
-
+echo "\n+++++++++++++++ Testing for a TRUE value"
 curl -i -H "Content-Type: application/json" -X POST -d '{"filesize":1000000,"password":"qwerty"}' -v http://127.0.0.1:100/query
-
-echo "------"
-echo "+++++++++++++++ Testing for a FALSE value"
-echo "------"
-
+echo "\n+++++++++++++++ Testing for a FALSE value"
 curl -i -H "Content-Type: application/json" -X POST -d '{"filesize":1000000,"password":"lkasfgy92bhvlis83gf"}' -v http://127.0.0.1:100/query
-
-echo "------"
-echo "+++++++++++++++ Testing for wrong POST data"
-echo "------"
-
+echo "\n+++++++++++++++ Testing for WRONG data"
 curl -i -H "Content-Type: application/json" -X POST -d '{"filesize":1000000,"pw":"lkasfgy92bhvlis83gf"}' -v http://127.0.0.1:100/query
